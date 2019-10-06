54cdf8449d8c87aab423a1d9700572d4d59221be
foolstuckdev <sagar.connect247@gmail.com>
2019-10-06 22:39:19 +0530

    Updates

---
+++ precache-manifest.ef19e11eb9300358c7417108e0882632.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60c0b94feb23799d1b268a97f801e62e",
+    "url": "/react-vidtube/index.html"
+  },
+  {
+    "revision": "c6122a5e0d467a88de1a",
+    "url": "/react-vidtube/static/css/main.fcc005ff.chunk.css"
+  },
+  {
+    "revision": "4f29ec9ff7e2f64e1c33",
+    "url": "/react-vidtube/static/js/2.98b5c135.chunk.js"
+  },
+  {
+    "revision": "c6122a5e0d467a88de1a",
+    "url": "/react-vidtube/static/js/main.e2ab911a.chunk.js"
+  },
+  {
+    "revision": "3909661c8ba951e09215",
+    "url": "/react-vidtube/static/js/runtime-main.338535ec.js"
+  }
+]);
